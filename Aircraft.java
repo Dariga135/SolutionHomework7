5d590550919618067a7cd360b0fc94d3fdbecc69
Dariga135 <dariga.dengelbayeva@narxoz.kz>
2025-04-29 17:11:19 +0500

    Part 2 – Mediator Pattern

---
+++ Aircraft.java
@@ -0,0 +1,23 @@
+public abstract class Aircraft {
+    protected String id;
+    protected int fuelLevel;
+
+    public Aircraft(String id, int fuelLevel) {
+        this.id = id;
+        this.fuelLevel = fuelLevel;
+    }
+
+    public abstract void receive(String msg);
+
+    public void send(String msg, TowerMediator m) {
+        m.broadcast(msg, this);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getFuelLevel() {
+        return fuelLevel;
+    }
+}
